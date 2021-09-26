b83e9ab32c166078a183e28ae48dae4d6a1694c2
Heath Bennett <heathben418@gmail.com>
2021-09-25 21:46:47 -0400

    teamStats steps 4 and 5 complete

--- teamStats.js
+++ teamStats.js
@@ -16,6 +16,32 @@ let team = {
     }
 
 ],
-    _games: []
+    _games: [{
+        opponent: 'Yankees',
+        teamScore: 10,
+        opponentScore: 6
+    },
+    {
+        opponent: 'Orioles',
+        teamScore: 15,
+        opponentScore: 3
+    },
+    {
+        opponent: 'Blue Jays',
+        teamScore: 8, 
+        opponentScore: 5   
+    }
+],
+    get teamPlayers () {
+        return this._players;
+    },
+
+    get teamGames () {
+        return this._games;
+    }
 
 };
+
+console.log(team._games[0]);
+console.log(team.teamPlayers);
+console.log(team.teamGames)
